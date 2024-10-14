22d685ab85938c612058760256681eeebd98c15f
Soe Moe Stephen <soe@frontware.co.th>
2024-10-15 06:29:30 +0700

    fix: port changed

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 load_dotenv()
 
 class Settings(BaseSettings):
-    MONGODB_URI: str = os.getenv("MONGODB_URI")
+    MONGODB_URI: str = os.getenv("MONGODB_URI", "mongodb+srv://schnatr:vcS0AQDhqKWSi6Tc@clusterzero.6uzvv.mongodb.net/?retryWrites=true&w=majority&appName=ClusterZero")
     DB_NAME: str = os.getenv("DB_NAME", "RESTAURANT")
     COLLECTION_NAME: str = os.getenv("COLLECTION_NAME", "RESTAURANT")
     
